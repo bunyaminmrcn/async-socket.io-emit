031f0b5907cba0f969f9e4a187864ddeff77db23
mrcnbunyamin <mrcnbunyamin@gmail.com>
2021-11-10 00:06:06 +0300

    Added main package

---
+++ index.js
@@ -0,0 +1,36 @@
+class AsyncSock {
+    /**
+     * 
+     * @param {any} sock 
+     */
+    constructor(sock) {
+        this.sock = sock;
+    }
+    /**
+     * 
+     * @param {string} topic 
+     * @param {object} payload 
+     * @param {function} callback 
+     * @returns {Promise}
+     */
+    emit = async (topic, payload, callback) => {
+        const promiseReturn = await new Promise(async (resolve, reject) => {
+            this.sock.emit(topic, payload, (data_) => {
+                resolve(data_)
+            })
+            this.sock.on('error', err => reject(err))
+        })
+
+        if(callback) {
+            try {
+                callback(promiseReturn)
+            } catch (error) {
+                return Promise.reject()
+            }
+        } else {
+            return promiseReturn;
+        }
+    }
+}
+
+module.exports = AsyncSock;
